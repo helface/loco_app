cc90ab0e3c9fb41eee65b8f32c3ee4c55653ebec
helen <helface@gmail.com>
2012-03-07 01:35:17 -0800

    debug

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ LocoApp::Application.routes.draw do
   
   match '/about', :to => 'pages#about'
   match '/find', :to => 'pages#find'
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -57,7 +58,6 @@ LocoApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
