6c5b40f85d4c710b849ba1207173a68c123e102d
helen <helface@gmail.com>
2012-03-12 12:06:40 -0700

    added password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,20 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  username   :string(255)
-#  firstname  :string(255)
-#  lastname   :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  id              :integer         not null, primary key
+#  username        :string(255)
+#  firstname       :string(255)
+#  lastname        :string(255)
+#  email           :string(255)
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#  password_digest :string(255)
 #
 
 class User < ActiveRecord::Base
   
-  attr_accessible :firstname, :lastname, :email, :username
+  attr_accessible :firstname, :lastname, :email, :username, :password, :password_confirmation
+  has_secure_password
   
   validates :username, presence: true, length:{maximum:24}, uniqueness:{case_sensitive: false}
   validates :firstname, presence:true, length:{maximum: 20}
@@ -23,10 +25,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format:{with:VALID_EMAIL_REGEX}, uniqueness: {case_sensitive:false}
 
-  #def initialize (attributes ={})
-   # @username = attributes[:username]
-    #@firstname = attributes[:firstname]
-    #@lastname = attributes[:lastname]
-    #@email = attributes[:email]
-  #end
+  validates :password, length:{minimum: 6}
+  validates :password_confirmation, presence: true
+  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,14 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  username   :string(255)
-#  firstname  :string(255)
-#  lastname   :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  id              :integer         not null, primary key
+#  username        :string(255)
+#  firstname       :string(255)
+#  lastname        :string(255)
+#  email           :string(255)
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#  password_digest :string(255)
 #
 
 require 'spec_helper'
