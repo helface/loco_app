8650c75fc261e47d5f1c1118c2a2ebf4726f01bb
helen <helface@gmail.com>
2012-03-30 17:08:10 +0000

    removing username from db

--- db/migrate/20120308075031_create_users.rb
+++ db/migrate/20120308075031_create_users.rb
@@ -1,11 +1,16 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :username
       t.string :firstname
       t.string :lastname
       t.string :email
-
+        
       t.timestamps
     end
+      
+      change_table :users do |t|
+          t.remove :username
+      end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120324173524) do
+ActiveRecord::Schema.define(:version => 20120330170138) do
 
   create_table "users", :force => true do |t|
-    t.string   "username"
     t.string   "firstname"
     t.string   "lastname"
     t.string   "email"
