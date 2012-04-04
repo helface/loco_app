a3bc6ff8d2d047c9d92b08f54729fff75266a3c3
helen <helface@gmail.com>
2012-04-04 15:36:24 +0000

    flash working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,16 +42,23 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+      #respond_to do |format|
+      #if @user.save
+      # format.html { redirect_to @user, notice: 'User was successfully created.' }
+      # format.json { render json: @user, status: :created, location: @user }
+      #else
+      # format.html { render action: "new" }
+      # format.json { render json: @user.errors, status: :unprocessable_entity }
         #render 'new'
-      end
+      #end
+      #end
+    if @user.save
+        flash[:success] = "welcome to locoo!"
+        redirect_to @user
+    else
+        render 'new'
     end
+          
   end
 
   # PUT /users/1
