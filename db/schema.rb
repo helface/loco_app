e8293c6476e3a4e5ad513298baf59a2d42d2158b
helen <helface@gmail.com>
2012-04-05 23:19:50 +0000

    new schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120405224959) do
+ActiveRecord::Schema.define(:version => 20120405231617) do
 
   create_table "users", :force => true do |t|
     t.string   "lastname"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20120405224959) do
     t.boolean  "admin",           :default => false
   end
 
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
