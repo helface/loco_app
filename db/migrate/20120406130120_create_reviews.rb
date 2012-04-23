c736d630526e3fbd2fcfa3751d3aeeafad04e6ca
helen <helface@gmail.com>
2012-04-23 15:27:58 +0000

    asdf

--- db/migrate/20120406130120_create_reviews.rb
+++ db/migrate/20120406130120_create_reviews.rb
@@ -2,11 +2,11 @@ class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
       t.string :content
-      t.integer :use_id
+      t.integer :user_id
 
       t.timestamps
     end
     
-      #add_index :reviews, [:user_id, :host_id, :created_at]
+      add_index :reviews, [:user_id, :host_id, :created_at]
   end
 end
