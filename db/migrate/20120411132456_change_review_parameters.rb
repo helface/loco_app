52d6c7065179e6b448b218d163990b0054d9c898
helen <helface@gmail.com>
2012-04-23 17:19:22 +0000

    fix

--- db/migrate/20120411132456_change_review_parameters.rb
+++ db/migrate/20120411132456_change_review_parameters.rb
@@ -2,7 +2,7 @@ class ChangeReviewParameters < ActiveRecord::Migration
   def change
     change_table :reviews do |t|
       t.rename :host_id, :reviewee_id
-      t.rename :user_id, :reviewer_id
+      t.rename :use_id, :reviewer_id
     end
   end
 end
