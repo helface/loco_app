639f9772ca5a936ca44e5e12b7cbd13b83e4943f
helen <helface@gmail.com>
2012-12-14 01:24:39 -0500

    edit

--- app/models/travelerreview.rb
+++ app/models/travelerreview.rb
@@ -2,5 +2,5 @@ class Travelerreview < ActiveRecord::Base
   attr_accessible :comment, :score
   belongs_to :user
   validates_presence_of :score
-  validates :comment, length:{maximum: 500}
+  validates :comment, length:{maximum: 2000}
 end
