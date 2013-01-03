e7f3eaa1a47f68fc446be7ec0cd9ae542033d074
helen <helface@gmail.com>
2013-01-03 01:22:17 -0500

    removing fb code

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,3 @@
-if Rails.env.development?
-  ENV["FACEBOOK_APP_ID"] = "401017706644512"
-  ENV["FACEBOOK_SECRET"] = "3d8f8632bc02c3ae41822fcce93a8b3a"
-end
-
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
