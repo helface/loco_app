6a555421fdccf8767876ccb8c170f6ff124e7b6a
helen <helface@gmail.com>
2013-01-06 18:23:05 -0500

    meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,22 @@ module ApplicationHelper
       "#{default_title} | #{page_title}"
     end
   end
+  
+  def default_keywords(keywords)
+    default_keywords = "travel, guides, locals, travelers, hosts, local guides, green travel"
+    if keywords.empty?
+      default_keywords
+    else
+      "#{default_keywords} #{keywords}"
+    end
+  end
+  
+  def default_description(desc)
+    default_description = "tiniHost connects travelers and locals for a more authentic, personal, and responsible travel experience."
+    if desc.empty?
+      default_description
+    else
+      desc
+    end
+  end
 end
