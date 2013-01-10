210fb1091c273c329837c49f380dd2fa949b099b
helen <helface@gmail.com>
2013-01-10 01:21:58 -0500

    set log level to error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ LocoApp::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  config.log_level = :warn
+  config.log_level = :error
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
